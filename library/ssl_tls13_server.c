253cb43c6ec456149d9c6ec3db90160fcc4cc9bb
Hannes Tschofenig <hannes.tschofenig@arm.com>
2021-07-29 09:04:23 +0200

    Guarding ssl_debug_print_client_hello_exts with MBEDTLS_DEBUG_C

--- library/ssl_tls13_server.c
+++ library/ssl_tls13_server.c
@@ -2267,12 +2267,14 @@ static int ssl_client_hello_fetch( mbedtls_ssl_context* ssl,
 
 #endif /* MBEDTLS_SSL_USE_MPS */
 
+#if !defined(MBEDTLS_DEBUG_C)
 static void ssl_debug_print_client_hello_exts( mbedtls_ssl_context *ssl )
 {
-#if !defined(MBEDTLS_DEBUG_C)
     ((void) ssl);
-#endif
-
+}
+#else
+static void ssl_debug_print_client_hello_exts( mbedtls_ssl_context *ssl )
+{
     MBEDTLS_SSL_DEBUG_MSG( 3, ( "Supported Extensions:" ) );
     MBEDTLS_SSL_DEBUG_MSG( 3, ( "- KEY_SHARE_EXTENSION ( %s )",
                                 ( ( ssl->handshake->extensions_present & MBEDTLS_SSL_EXT_KEY_SHARE ) > 0 ) ?
@@ -2318,6 +2320,7 @@ static void ssl_debug_print_client_hello_exts( mbedtls_ssl_context *ssl )
                                 "TRUE" : "FALSE" ) );
 #endif /* MBEDTLS_ZERO_RTT*/
 }
+#endif /* !MBEDTLS_DEBUG_C */
 
 static int ssl_client_hello_has_psk_extensions( mbedtls_ssl_context *ssl )
 {
